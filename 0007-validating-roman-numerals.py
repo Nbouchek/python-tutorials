3c2ff52478ba257d561012914b5d0551c16a8c43
Nacer Bouchekhima <nbouchek@gmail.com>
2020-04-13 11:52:43 +0100

    Vlidating phone numbers using regular expressions - ruby style

--- 0007-validating-roman-numerals.py
+++ 0007-validating-roman-numerals.py
@@ -24,8 +24,8 @@
 #
 # https://developers.google.com/edu/python/regular-expressions
 
-regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"  # Do not delete 'r'.
-
 import re
+
+regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"  # Do not delete 'r'.
 
 print(str(bool(re.match(regex_pattern, input()))))
