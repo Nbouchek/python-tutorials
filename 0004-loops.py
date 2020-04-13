c5f77bda0fbd3fc83e24c8b82ce8af9f9636f335
Nacer Bouchekhima <nbouchek@gmail.com>
2020-04-13 10:00:20 +0100

    Loops

---
+++ 0004-loops.py
@@ -0,0 +1,29 @@
+# Task
+# Read an integer N. For all non-negative integers i < N, print i^2. See the sample for details.
+#
+# Input Format
+#
+# The first and only line contains the integer, .
+#
+# Constraints
+#
+#
+# Output Format
+#
+# Print  lines, one corresponding to each .
+#
+# Sample Input 0
+#
+# 5
+# Sample Output 0
+#
+# 0
+# 1
+# 4
+# 9
+# 16
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i ** 2)
